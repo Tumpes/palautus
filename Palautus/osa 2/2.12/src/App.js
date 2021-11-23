ddba1ee2f950f214e50b7292a9b24926248b048c
Tumpess <Tuomas.maristo@eduespoo.fi>
2021-11-23 14:33:36 +0200

    2.14

--- Palautus/osa 2/2.12/src/App.js
+++ Palautus/osa 2/2.12/src/App.js
@@ -1,15 +1,90 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+const Countries = (props) => {
+  const [weather, setWeather] = useState({});
+  const matches = props.matches;
+  console.log(matches.length);
+  console.log(matches);
+
+  const temp = matches[0] ? matches[0] : matches;
+  const country = temp.name ? temp.name.common : temp;
+
+  console.log(country);
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://api.weatherstack.com/current?access_key=${props.api_key}&query=${country}`
+      )
+      .then((response) => {
+        setWeather(response);
+      });
+  }, [country, props.api_key]);
+
+  if (matches.length === 0) return <p> Ei hakutuloksia. </p>;
+  else if (matches.length >= 10) return <p> Tarkenna hakua. </p>;
+  else if (matches.length === 1 || matches.length === undefined) {
+    const country = matches[0] ? matches[0] : matches;
+    console.log(country.name.common);
+
+    return (
+      <>
+        <h1> {country.name.common} </h1>
+        <p>capital {country.capital}</p>
+        <p> continent {country.continents} </p>
+        <h3> languages </h3>
+        {Object.values(country.languages).map((language) => {
+          return <p key={language}> {language} </p>;
+        })}
+        <img src={country.flags.png} alt="maan lippu" />
+        <p> temperature: {weather.data.current.temperature}</p>
+        <img
+          src={weather.data.current.weather_icons[0]}
+          alt="kuva säästä"
+        ></img>
+        <p>
+          wind: {weather.data.current.wind_speed}mph direction
+          {weather.data.current.wind_degree}
+        </p>
+      </>
+    );
+  } else {
+    console.log(matches);
+    return matches.map((country) => {
+      return (
+        <p key={country.name.common}>
+          {country.name.common}
+          <button
+            onClick={() => {
+              props.setMatches(country);
+            }}
+          >
+            show
+          </button>
+        </p>
+      );
+    });
+  }
+};
+
 const App = () => {
+  const api_key = process.env.REACT_APP_API_KEY;
+  console.log(api_key);
+
   const [search, setSearch] = useState("");
-  const [matches, setMatches] = useState("");
-  const [countries, setCountries] = useState("");
+  const [matches, setMatches] = useState([]);
+  const [countries, setCountries] = useState([]);
 
   const handleSearchChange = (event) => {
     console.log(event.target.value);
     setSearch(event.target.value);
-    setMatches(countries.filter((e) =>  e.name.common === event.target.value));
+    console.log(countries);
+    setMatches(
+      countries.filter((e) =>
+        e.name.common.toLowerCase().includes(event.target.value)
+      )
+    );
     console.log(countries.filter((e) => e.name.common === event.target.value));
   };
 
@@ -24,7 +99,11 @@ const App = () => {
     <div>
       find countries
       <input onChange={handleSearchChange} value={search}></input>
-      <p> {matches} </p>
+      <Countries
+        matches={matches}
+        setMatches={setMatches}
+        api_key={api_key}
+      ></Countries>
     </div>
   );
 };
